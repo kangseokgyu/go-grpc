645dd4c1548f66b763f3933633afe285d8801425
kangseokgyu <ksg890528@gmail.com>
2022-08-12 13:46:12 +0900

    url의 opengraph 정보 가져오기

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,14 +3,42 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/dyatlov/go-opengraph/opengraph"
 )
 
 var (
-	name = flag.String("name", "world", "Name")
+	url = flag.String("url", "http://naver.com", "request url")
 )
 
 func main() {
 	flag.Parse()
 
-	fmt.Printf("Hello %s.\n", *name)
+	resp, err := http.Get(*url)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	og := opengraph.NewOpenGraph()
+	err = og.ProcessHTML(strings.NewReader(string(data)))
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("Type: %s\n", og.Type)
+	fmt.Printf("Title: %s\n", og.Title)
+	fmt.Printf("URL: %s\n", og.URL)
+	fmt.Printf("String/JSON Representation: %s\n", og)
 }
